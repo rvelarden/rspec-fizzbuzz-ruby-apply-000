38293b586dd283c41663f3a2b31c0a0f1cf65b33
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-22 02:02:19 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,13 +4,12 @@ require_relative './spec_helper.rb'
 
 def fizzbuzz(number)
 if number % 3 == 0 && number % 5 == 0
-puts "FizzBuzz"
+"FizzBuzz"
 elsif number % 5 == 0
-puts "Buzz"
+"Buzz"
 elsif number % 3 == 0
-puts "Fizz"
-else 
-
+"Fizz"
+else
 puts nil
 end
 end
