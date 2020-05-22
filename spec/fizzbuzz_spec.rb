c17496505d9a0417423badf5fd85f2a045276e6b
Romina Velarde <rominapaola.velarde@gmail.com>
2020-05-22 01:51:02 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,7 +5,7 @@ require_relative './spec_helper.rb'
 def fizzbuzz(number)
 if number % 3 == 0
 puts "Fizz"
-elsif number % 3 == 0
+elsif number % 5 == 0
 puts "Buzz"
 elsif number % 3 == 0 && number % 5 == 0
 puts "FizzBuzz"
